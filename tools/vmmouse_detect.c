c4ec4b582a48b751a28777419860abf182197c5d
Alan Coopersmith <alan.coopersmith@oracle.com>
2025-08-12 16:41:06 -0700

    Strip trailing whitespace from source files
    Performed with: `git ls-files | xargs perl -i -p -e 's{[ \t]+$}{}'`
    
    `git diff -w` & `git diff -b` show no diffs from this change
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Part-of: <https://gitlab.freedesktop.org/xorg/driver/xf86-input-vmmouse/-/merge_requests/7>

--- tools/vmmouse_detect.c
+++ tools/vmmouse_detect.c
@@ -57,7 +57,7 @@ main(void)
     */
    signal(SIGSEGV, segvCB);
 
-#if defined __i386__ || defined __x86_64__ 
+#if defined __i386__ || defined __x86_64__
    (void) xf86EnableIO();
    if (VMMouseClient_Enable()) {
       VMMouseClient_Disable();
